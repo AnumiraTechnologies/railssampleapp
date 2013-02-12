c057742e2fa389bc4a10ca368dfa4fac6e13a7e2
Anumira Technologies <administrator@anumira.com>
2013-02-12 14:43:41 +0530

    railssampleapplication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samplesite::Application.config.secret_token = 'a8073683acf74e3fd7a08dee9da60ff4396eb519ecbfbc26f3243604deea177eea4c05369b45012a135fbc9806fa0ee73b0a167d2e57dc78f1efcd50840bf19b'
